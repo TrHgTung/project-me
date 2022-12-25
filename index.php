ef5c1be38416e54460837f7ab7c0c633e40f6351
TrHgTung <89836403+TrHgTung@users.noreply.github.com>
2022-12-25 01:30:40 -0800

    optimizing into MVC model

--- index.php
+++ index.php
@@ -1,21 +1,27 @@
 <?php
-    session_start();
 
-    include("connection.php");
-    include("functions.php");
-
-   // $user_data = check_login($con);
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
+  <head>
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
     <link rel="stylesheet" type="text/css" href="css/style.css" >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+            rel="stylesheet" 
+            integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" 
+            crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="images/favicon.png">
+    <title>Home Page</title>
     <style>
         @keyframes mountains_behind{
             0%   {left:-200px; top:400px;}
@@ -38,18 +44,12 @@
             100%  { opacity: 1; top: 0px}
         }
         @keyframes little-starters-pkm{
-            0%   {opacity: 0; }
-            25%   {opacity: 0; }
-            50%   {opacity: 0; }
+       
             75%   {opacity: 0; }
             80%   {opacity: 0.5; }
             100%  { opacity: 1; }
         }
         @keyframes atext{
-            0% {opacity: 0;}
-            25%   {opacity: 0; }
-            50%   {opacity: 0; }
-            75%   {opacity: 0; }
             80%   {opacity: 0.5; }
             100% {opacity: 1;}
         }
@@ -61,34 +61,37 @@
             0%  {opacity: 0;  top:-100px; left:-40px}
             30% {opacity: 0.5; top: 0px; left: 0px}
             60% {opacity: 1;}
-            100%    {top:0px; left: 0px}
+            100% {top:0px; left: 0px}
 
         }
     </style>
-</head>
-<body>
+  </head>
+
+  <body>
     <header>
         <a href="https://en.wikipedia.org/wiki/Pok%C3%A9mon" class="logo">pokémon</a>
         <ul>
             <li><a href="user.php" class="active">Home</a></li>
             <li><a href="login.php">Login</a></li>
             <li><a href="about-me.php">About Me!</a></li>
-            
         </ul>
     </header>
     <section>
         <img src="images/stars.png" alt="" id="stars">
         <img src="images/moon.png" alt="" id="moon">
         <img src="images/mountains_behind.png" alt="" id="mountains_behind">
-        <h2 id="text">Welcome</h2>
-        
-        <a href="login.php" id="btn">to the Pokémon world</a>
         <img src="images/mountains_front.png" alt="" id="mountains_front">
-    
         <img src="images/little-pkm.png" alt="" id="little-pkm">
         <img src="images/little-starters-pkm.png" alt="" id="little-starters-pkm">
         
-
+        <div>
+            <div >
+                <p class="fs-1 text text-white">Welcome</p>
+            </div>
+            <div>
+                <a href="login.php" class="btn btn-primary button" role="button" >to the Pokémon world</a>
+            </div>  
+        </div>
     </section>
     <footer class="footer">
         <div class="grid footer__container">
@@ -99,12 +102,7 @@
                 <ul class="foot-item-box">
                     <li>Pokémon (an abbreviation for Pocket Monsters in Japan) is a Japanese media franchise managed by The Pokémon Company, Game Freak, and Creatures. The franchise was created by Satoshi Tajiri in 1996 </li>
                     <li>The English slogan for the franchise is "Gotta Catch ‘Em All!". There are currently 913 Pokémon species (2019).</li>
-                    <li>Email: <a href="mailto:tungng14@gmail.com"><b>tungng14@gmail.com</b></a></li>
-                    <!-- 
-                     and is centered on fictional creatures called "Pokémon". In Pokémon, humans, known as Pokémon Trainers, 
-                     catch and train Pokémon to battle other Pokémon for sport. All media works within the franchise are set in the Pokémon universe. 
-                     --->
-                    
+                    <li>Email: <a href="mailto:tungng14@gmail.com"><b>tungng14@gmail.com</b></a></li>               
                 </ul>
             </li>
             <li>
@@ -130,8 +128,5 @@
             </ul>
         </div>
     </footer>
-
-        
-    
-</body>
+  </body>
 </html>
