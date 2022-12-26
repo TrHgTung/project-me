11cd069d73324ab1d2d269ce1c8c71229bf97763
tung <tungng14@gmail.com>
2022-12-26 22:09:44 +0700

    added some images and finished the first news page

--- news/child-news/news-01.php
+++ news/child-news/news-01.php
@@ -1,5 +1,5 @@
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -8,7 +8,7 @@
     <meta name="author" content="Hoang Tung">
     <meta name="generator" content="">
     <link rel="icon" type="image/x-icon" href="../../images/favicon.png">
-    <title>Nov 15th, 2022 - Ash Ketchum won the finale in The Pokémon World Championship</title>
+    <title>Nov 11th, 2022 - Ash Ketchum won the finale in The Pokémon World Championship</title>
 
     <link href="../../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
@@ -19,23 +19,24 @@
     <link href="../../css/carousel.css" rel="stylesheet">
     <style>
       .heading-01{
-        position: relative;
+        position: absolute;
         filter: brightness(40%);
         pointer-events: none;
         user-select: none;
       }
-      .bgColor{
-        background-color: white;
-        color:black;
-        border-bottom:1px solid lightgray;
-        transition: all 1s;
+      .text-article,.text {
+        color: #000;
+        font-size:18px;
       }
-      #main-nav{
-        background: transparent;
-        transition: background 0.3s;
-      }
-      .scrolled #main-nav {
-          background: black;
+      .text-article:first-letter {
+          float:left;
+          font-weight: bold;
+          font-size: 60px;
+          font-size: 6rem; 
+          line-height: 40px;
+          line-height: 4rem;
+          height:4rem;
+          text-transform: uppercase;
       }
     </style>
 
@@ -45,27 +46,22 @@
         function changeBg (){
           var nav01 = document.getElementById('nav01');
           var scrollValue = window.scrollY;
-          console.log(scrollValue); //need to change on ~186 (= y = 186.**** px)  559.2000122070312
-            if(scrollValue > 186.39999389648438 && scrollValue < 559.2000122070312){
-              nav01.classList.remove('navbar navbar-expand-md navbar-dark fixed-top bg-dark');
-            }else{
-              nav01.classList.add('navbar navbar-expand-md navbar-dark fixed-top bg-dark');
+          // console.log(scrollValue); //need to change on ~186 (= y = 186.**** px) && scrollValue < 559.2000122070312
+            if(scrollValue > 186 && scrollValue < 559){
+              nav01.classList.remove("bg-dark");
+            } else{
+              nav01.classList.add("bg-dark");
             }
-        }
-        window.addEventListener('scroll', changeBg);
-
-        $(function() {
-          $(window).on("scroll", function() {
-            $("body").toggleClass("scrolled", $(document).scrollTop() > 50);
-          })
-        });
+          }
+          window.addEventListener('scroll', changeBg);
+        
     </script>
 
   </head>
   <body oncontextmenu="return false;">  
    <header>
   <!-- nav bar 01 __id -->
-    <nav id="nav01" class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <nav id="nav01" class="navbar navbar-expand-md navbar-dark fixed-top py-2 bg-dark">
      <div class="container-fluid">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -94,31 +90,19 @@
 <main>
 
   <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-    <!-- <div class="carousel-indicators">
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    </div> -->
-    <div class="carousel-inner">
+    <div class="carousel-inner ">
       <div class="carousel-item active">
-      <img src="../images-news/heading-01.jpg" alt="" width="100%" height="100%" class="heading-01" aria-hidden="true" focusable="false">  
-      <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg> -->
+      <img src="../images-news/heading-04.jpg" alt="" width="100%" height="100%" class="heading-01" aria-hidden="true" focusable="false">  
         <div class="container">
           <div class="carousel-caption text-start">
             <h1>After 25 Years, Ash Ketchum Finally Becomes Pokemon World Champion.</h1>
-            <p>Over 25 years since the Pokemon animated series first premiered, protagonist Ash Ketchum has finally achieved his dream of becoming a World Champion.</p>
+            <div>
+              <p>Over 25 years since the Pokemon animated series first premiered, protagonist Ash Ketchum has finally achieved his dream of becoming a World Champion. &nbsp;&nbsp;&nbsp;</p><small><b>Dịch bởi:</b> <i>Hoàng Tùng</i></small>
+            </div>
             <!-- <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>  -->
           </div>
         </div>
       </div>
-      <!-- <div class="carousel-item">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
-        <div class="container">
-          <div class="carousel-caption">
-            <h1>Another example headline.</h1>
-            <p>Some representative placeholder content for the second slide of the carousel.</p>
-            <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
-          </div>
-        </div>
-      </div> -->
       <div class="carousel-item">
         <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
         <div class="container">
@@ -130,14 +114,6 @@
         </div>
       </div>
     </div>
-    <!-- <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button> -->
-    <!-- <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button> -->
   </div>
 
 
@@ -149,24 +125,12 @@
 
     <!-- Three columns of text below the carousel -->
     <div class="row">
-      <!-- <div class="col-lg-4">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-        <h2 class="fw-normal">Heading</h2>
-        <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
-      </div>
-      <div class="col-lg-4">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-        <h2 class="fw-normal">Heading</h2>
-        <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
-      </div> -->
       <div class="d-flex align-items-center justify-content-center">
         <div class="col-lg-4">
           <img src="../../images/lucario.jpg"  class="bd-placeholder-img rounded-circle"  border-radius="50%" width="80" height="80" focusable="false"><rect width="100%" height="100%" fill="#777"/></img>
           <h6 class="fw-normal mt-2"><strong>Tung is the author of this article</strong> </h6>
           <small>
-            <p class="mb-2">Last updated: Dec 26th, 2022</p>
+            <p class="mb-2">Last updated: Dec 28th, 2022</p>
           </small>
           <p><a class="btn btn-secondary" href="../../about-me.php">View details &raquo;</a></p>
         </div>
@@ -175,16 +139,48 @@
 
 
     <!-- START THE FEATURETTES -->
+    <div>
+      <div class="text-article mt-5">During the latest episode of the Pokemon Ultimate Journeys series in Japan,
+          Ash defeated Galar champion Leon in the final round of the Masters 
+          Eight Tournament to earn the title of top trainer in the Pokemon world.</div>
+      <div>&nbsp;
+    </div>
+    <div>
+        <div class="text mt-5">
+          <i>"Ash Ketchum's determination and perseverance to achieve his goal of becoming the world's top
+          Pokemon Trainer over the course of 25 seasons 
+          represents the very best of what it means to be a Trainer," </i>Taito Okiura, 
+          vice president of marketing for Pokemon Company International, said in a press release Friday.
+        </div>
+    </div>
+    <div>
+      <div class="text mt-5">
+        The Pokemon Ultimate Journeys series is streaming in the US exclusively on Netflix.
+        The finals of the Masters Eight Tournament will air worldwide "in the future."
+      </div>
+    </div>
+    <div>
+      <div class="text mt-5">
+      Ash's victory comes just a week before the launch of the latest mainline Pokemon games, 
+      Pokemon Scarlet and Violet. The pair arrive for Nintendo Switch on Nov. 18 and take players to a 
+      brand-new open-world region called Paldea, which is home to a variety of new Pokemon. 
+      Everyone who purchases one of the games before 
+      Feb. 28 will be able to receive a special bonus Pikachu.
+      </div>
+    </div>
 
     <hr class="featurette-divider">
 
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading fw-normal lh-1">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+        <div class="featurette-heading fw-normal lh-1 mb-3">TPC has tweeted about the victory of Ash</div>
+        <p class="lead"><i>"He's done it! Ash has become a World Champion!</i> (icon)(icon)".
+         That's what TPC (The Pokémon Company) talk about our little young man, 
+        a 10 year-old trainer with 25 years experience in training and battling Pokémon.</p>&nbsp;
+        <p class="lead">You can watch it <a href="https://twitter.com/Pokemon/status/1591015158016446464/" target="_blank">here</a></p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <img src="../images-news/heading-03.jpg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="750" height="1334" focusable="false"></img>
       </div>
     </div>
 
@@ -192,11 +188,13 @@
 
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+        <div class="featurette-heading fw-normal lh-1 mb-3">Talk about the line-up</div>
+        <p class="lead">With all the effort, loyalty and hard-training, Dragonite, Gengar, Lucario, Sirfetch'd (Galarian), Dracovish and Pikachu have
+          brought the most sastified, colorful and emotional battle for every single fan who loves Pokémon, and has been waited for the moment that Ash take the world master trainer achievement
+        </p>
       </div>
       <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+        <img src="../images-news/heading-02.jpg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="1200" height="629" ></img>
       </div>
     </div>
 
@@ -204,19 +202,31 @@
 
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading fw-normal lh-1">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
+        <h2 class="featurette-heading fw-normal lh-1 mb-3">A minute flash-back with Ash </h2>
+        <div class="lead">Ash is a young man come from Kanto, with the dream that encounter all the Pokémon and be friend with all trainer all around the world.</div>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+      <img src="../images-news/heading-05.jpg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="1200" height="675" ></img>
       </div>
     </div>
 
     <hr class="featurette-divider">
 
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <div class="featurette-heading fw-normal lh-1 mb-3">Battling style</div>
+        <p class="lead">Let me show you the way Ash controlled and conqueued the battle. We saw a young boy with
+          a crazy tactic, Ash and his Pokémon had been fighting together, had a same thinking, and it let only one goal, the winning for winner!
+          
+        </p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <img src="../images-news/heading-01.jpg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="1200" height="629" ></img>
+      </div>
+    </div>
+    <hr class="featurette-divider">
     <!-- /END THE FEATURETTES -->
-
-  </div><!-- /.container -->
+  </div>
 
 </main>
 
